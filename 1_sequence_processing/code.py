a92a4ffdad5f7d6eba74fe4464b95d07c89583bc
shimizumasahiro <michiooo0506@gmail.com>
2024-04-03 17:34:02 -0700

    課題1-1

--- 1_sequence_processing/code.py
+++ 1_sequence_processing/code.py
@@ -1,10 +1,22 @@
 from typing import List, Union
 import numpy.typing as npt
 import numpy as np
+from Bio import SeqIO
+import inspect
+
+def read_fasta_file(fastapath: str) -> SeqIO.FastaIO:
+    sequences = []
+    for record in SeqIO.parse(fastapath, "fasta"):
+        sequences.append(record)
+    return sequences
 
 def base_count(fastafile: str) -> List[int]:
+    # データの読み込み
+    file_data = read_fasta_file(fastafile)
+    sequences = file_data[0].seq
+
     # 課題 1-1
-    return [0, 0, 0, 0] # A, T, G, C
+    return [sequences.count("A"), sequences.count("T"), sequences.count("G"), sequences.count("C")] # A, T, G, C
 
 def gen_rev_comp_seq(fastafile: str) -> str:
     # 課題 1-2
@@ -27,11 +39,11 @@ if __name__ == "__main__":
     filepath = "data/NT_113952.1.fasta"
     # 課題 1-1
     print(base_count(filepath))
-    # 課題 1-2
-    print(gen_rev_comp_seq(filepath))
-    # 課題 1-3
-    print(calc_gc_content(filepath))
-    # 課題 1-4
-    print(search_motif(filepath, "ATG"))
-    # 課題 1-5
-    print(translate(filepath))
+    # # 課題 1-2
+    # print(gen_rev_comp_seq(filepath))
+    # # 課題 1-3
+    # print(calc_gc_content(filepath))
+    # # 課題 1-4
+    # print(search_motif(filepath, "ATG"))
+    # # 課題 1-5
+    # print(translate(filepath))
