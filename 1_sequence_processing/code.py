ad496b1add55bcbbd04c47c46173044b2ad6d80a
shimizumasahiro <michiooo0506@gmail.com>
2024-04-03 18:30:05 -0700

    課題1-3

--- 1_sequence_processing/code.py
+++ 1_sequence_processing/code.py
@@ -3,6 +3,7 @@ import numpy.typing as npt
 import numpy as np
 from Bio import SeqIO
 import inspect
+import matplotlib.pyplot as plt
 
 def read_fasta_file(fastapath: str) -> SeqIO.FastaIO:
     sequences = []
@@ -28,8 +29,21 @@ def gen_rev_comp_seq(fastafile: str) -> str:
 
 def calc_gc_content(fastafile: str, window: int=1000, step: int=300) -> Union[npt.NDArray[np.float_], List[float]]:
     # 課題 1-3
+    # データの読み込み
+    file_data = read_fasta_file(fastafile)
+    sequences = file_data[0].seq
+
+    # 処理
+    gc_contents = []
+    index_list = []
+    for i in range(0, len(sequences) - window + 1, step):
+        subseq = sequences[i:i+window]
+        gc_count = subseq.count("G") + subseq.count("C")
+        gc_content = (gc_count / window) * 100
+        gc_contents.append(gc_content)
+        #index_list.append(i)
     # 値を出力するところまで。matplotlibを使う部分は別途実装してください。
-    return []
+    return np.round(gc_contents, 2)#, index_list
 
 def search_motif(fastafile: str, motif: str) -> List[str]:
     # 課題 1-4
@@ -45,8 +59,11 @@ if __name__ == "__main__":
     print(base_count(filepath))
     # 課題 1-2
     print(gen_rev_comp_seq(filepath))
-    # # 課題 1-3
-    # print(calc_gc_content(filepath))
+    # 課題 1-3
+    print(calc_gc_content(filepath))
+    # plt.plot(index, result)
+    # plt.savefig("課題1-3.pdf")
+
     # # 課題 1-4
     # print(search_motif(filepath, "ATG"))
     # # 課題 1-5
